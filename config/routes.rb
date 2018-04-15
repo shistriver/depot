155afdbcfad2878ce66ed781478a917d65a2368e
shiyuanzhong <shiyuanzhong@sspaas.com>
2018-04-15 21:20:27 +0800

    add store module

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  #get 'store/index'
+  #目的是让rails创建store_index_path 和 store_index_url 两个方法以便现有代码（和测试）能继续正常运行
+  root 'store#index', as: 'store_index'
+
   resources :products
   get 'say/hello'
 
